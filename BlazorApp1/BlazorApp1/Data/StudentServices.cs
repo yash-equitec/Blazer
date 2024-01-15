626e8a9632d078196a39815c710c3fcdd774cec9
yash-equitec <yash.equitec@gmail.com>
2024-01-15 20:35:48 +0530

    init

---
+++ BlazorApp1/BlazorApp1/Data/StudentServices.cs
@@ -0,0 +1,30 @@
+﻿// StudentService.cs
+using BlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class StudentService
+{
+    private readonly StudentDBContext _dbContext;
+
+    public StudentService(StudentDBContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public async Task<List<StudentTable>> GetStudentDataAsync()
+    {
+        try
+        {
+            return await _dbContext.StudentTables.FromSqlRaw("EXEC StudentView").ToListAsync();
+        }
+        catch (Exception ex)
+        {
+         
+            Console.WriteLine($"Error fetching student data: {ex.Message}");
+            throw; 
+        }
+    }
+}
